5b9f904b890228ac94296d1d8f52e8fd4500829d
Ales Kozumplik <kozumplik@gmail.com>
2023-09-28 22:16:36 +0100

    Latest cats-effect.

--- src/Deca.scala
+++ src/Deca.scala
@@ -1,6 +1,6 @@
 //> using dep com.monovore::decline::2.4.1
 //> using dep org.typelevel::cats-core::2.10.0
-//> using dep org.typelevel::cats-effect::3.5.1
+//> using dep org.typelevel::cats-effect::3.5.2
 
 import cats.Show
 import cats.data.State
