79f5f9ec8a9a0799f712238ad8a1cadb272701e3
Ales Kozumplik <kozumplik@gmail.com>
2024-01-14 08:23:58 +0000

    build: Move to Scala 3.3.1, properly checks for unused imports.

--- src/decadeka/project.scala
+++ src/decadeka/project.scala
@@ -1,7 +1,6 @@
+//> using scala 3.3.1
+//> using option -Wunused:all
+
 //> using dep com.monovore::decline::2.4.1
 //> using dep org.typelevel::cats-core::2.10.0
 //> using dep org.typelevel::cats-effect::3.5.2
-// testing:
-//> using dep com.disneystreaming::weaver-cats::0.8.3
-
-//> using option -Wunused:all
---
+++ src/decadeka/project.test.scala
@@ -0,0 +1,1 @@
+//> using dep com.disneystreaming::weaver-cats::0.8.3
