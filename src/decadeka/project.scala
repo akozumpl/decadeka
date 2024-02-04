1032df1665e1a543e0d702e73e3a0ce17c65e5f3
Ales Kozumplik <kozumplik@gmail.com>
2024-02-04 07:30:09 +0000

    Bump deps.

--- src/decadeka/project.scala
+++ src/decadeka/project.scala
@@ -3,4 +3,4 @@
 
 //> using dep com.monovore::decline::2.4.1
 //> using dep org.typelevel::cats-core::2.10.0
-//> using dep org.typelevel::cats-effect::3.5.2
+//> using dep org.typelevel::cats-effect::3.5.3
--- src/decadeka/project.test.scala
+++ src/decadeka/project.test.scala
@@ -1,1 +1,1 @@
-//> using dep com.disneystreaming::weaver-cats::0.8.3
+//> using dep com.disneystreaming::weaver-cats::0.8.4
